eefd661990cfd5fb6f68f207d76b6cdc11c5bd4f
Bree Stanwyck <bree@bignerdranch.com>
2014-12-01 14:58:31 -0500

    Working typeclass version
    * Other one got borked during the refactor. Should be running the test file more often :)

--- src/Unification.hs
+++ src/Unification.hs
@@ -3,16 +3,7 @@ module Unification where
 import Control.Applicative ((<$>),(<*>))
 import Control.Monad.State (get,put,modify, runStateT)
 import Types (Equation(..),Type(..),StackType(..))
-import Unification.Types (Unifier(..))
-
-addEqn :: Equation -> Unifier ()
-addEqn e = Unifier (modify (++[e]))
-
-getEqns :: Unifier [Equation]
-getEqns = Unifier get
-
-clearEqns :: Unifier ()
-clearEqns = Unifier (put [])
+import Unification.Types
 
 unify :: Type -> [Equation] -> Maybe Type
 unify t es = untilSame (unifyIter es t)
@@ -32,70 +23,12 @@ unify' (e:es) t = unifyStep t e >>= unify' es
 
 unifyStep :: Type -> Equation -> Unifier Type
 unifyStep t e = let s :~ s' = e in case (s,s') of
-  ([] :# a, _) -> substituteStackInType a s' t
+  ([] :# a, _) -> subst a s' t
   (_, [] :# _) -> unifyStep t (s' :~ s)
   ((l:as) :# a, (r:bs) :# b) -> do es <- getEqns
                                    clearEqns
-                                   es' <- mapM (substituteTypeInEquation l r) es
+                                   es' <- mapM (subst l r) es
                                    mapM_ addEqn es'
-                                   t' <- substituteTypeInType l r t
+                                   t' <- subst l r t
                                    t'' <- unifyStep t' ((as :# a) :~ (bs :# b))
                                    return t''
-
-substituteTypeInType :: Type -> Type -> Type -> Unifier Type
-substituteTypeInType l r t
-  | l == r = return t
-  | l `occursIn` r = fail "Cyclic type"
-  | otherwise = case (l,r) of
-                  (TVar n,_) -> return $ substituteTVarInType n r t
-                  (_,TVar _) -> substituteTypeInType r l t
-                  (Fun a b, Fun c d) -> do mapM_ addEqn [a :~ c, b :~ d]
-                                           return t
-                  _ -> return t
-
-substituteTVarInType :: Int -> Type -> Type -> Type
-substituteTVarInType n r t = case t of
-  TVar n' | n == n'   -> r
-          | otherwise -> t
-  Fun (as :# a) (bs :# b) -> let as' = map (substituteTVarInType n r) as
-                                 bs' = map (substituteTVarInType n r) bs
-                             in Fun (as' :# a) (bs' :# b)
-  _ -> t
-
-occursIn :: Type -> Type -> Bool
-occursIn t t'
-  | t == t' = True
-  | otherwise = case t' of
-                  Fun (as :# _) (bs :# _) -> any (t `occursIn`) (as ++ bs)
-                  _ -> False
-
-stackOccursIn :: Int -> StackType -> Bool
-stackOccursIn n (as :# a) = n == a || any stackOccursIn' as
-  where stackOccursIn' t = case t of
-          Fun (xs :# x) (ys :# y) -> n == x || n == y || any stackOccursIn' (xs++ys)
-          _ -> False
-
-substituteTypeInEquation :: Type -> Type -> Equation -> Unifier Equation
-substituteTypeInEquation l r (a :~ b) = do a' <- substituteTypeInStack l r a
-                                           b' <- substituteTypeInStack l r b
-                                           return $ a' :~ b'
-
-substituteTypeInStack :: Type -> Type -> StackType -> Unifier StackType
-substituteTypeInStack l r (as :# a) = do as' <- mapM (substituteTypeInType l r) as
-                                         return (as' :# a)
-
-substituteStackInType :: Int -> StackType -> Type -> Unifier Type
-substituteStackInType n a _ | n `stackOccursIn` a = fail "Cyclic type"
-substituteStackInType n a t = case t of
-  Fun b c -> Fun <$> (substituteStackInStack n a b) <*> (substituteStackInStack n a c)
-  _ -> return t
-
-substituteStackInStack :: Int -> StackType -> StackType -> Unifier StackType
-substituteStackInStack n (as :# a) (xs :# x) = do
-  xs' <- mapM (substituteStackInType n (as :# a)) xs
-  return $ if x == n
-    then (xs' ++ as) :# a
-    else xs' :# x
-
-substituteStackInEquation :: Int -> StackType -> Equation -> Unifier Equation
-substituteStackInEquation n s (a :~ b) = (:~) <$> substituteStackInStack n s a <*> substituteStackInStack n s b
--- src/Unification/Types.hs
+++ src/Unification/Types.hs
@@ -1,11 +1,81 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, MultiParamTypeClasses #-}
 
 module Unification.Types where
 
 import Types
 
 import Control.Applicative
-import Control.Monad.State (StateT)
+import Control.Monad.State
 
 newtype Unifier a = Unifier { runUnifier :: StateT [Equation] Maybe a }
   deriving (Functor, Applicative, Monad)
+
+class Subst l r t where
+  subst :: l -> r -> t -> Unifier t
+
+instance Subst Type Type Type where
+  subst l r t
+    | l == r = return t
+    | l `occursIn` r = fail "Cyclic type"
+    | otherwise = case (l,r) of
+                    (TVar n,_) -> subst n r t
+                    (_,TVar _) -> subst r l t
+                    (Fun a b, Fun c d) -> do mapM_ addEqn [a :~ c, b :~ d]
+                                             return t
+                    _ -> return t
+
+instance Subst Int Type Type where
+  subst n r t = case t of
+    TVar n' | n == n'   -> return r
+            | otherwise -> return t
+    Fun (as :# a) (bs :# b) -> do as' <- mapM (subst n r) as
+                                  bs' <- mapM (subst n r) bs
+                                  return $ Fun (as' :# a) (bs' :# b)
+    _ -> return t
+
+occursIn :: Type -> Type -> Bool
+occursIn t t'
+  | t == t' = True
+  | otherwise = case t' of
+                  Fun (as :# _) (bs :# _) -> any (t `occursIn`) (as ++ bs)
+                  _ -> False
+
+stackOccursIn :: Int -> StackType -> Bool
+stackOccursIn n (as :# a) = n == a || any stackOccursIn' as
+  where stackOccursIn' t = case t of
+          Fun (xs :# x) (ys :# y) -> n == x || n == y || any stackOccursIn' (xs++ys)
+          _ -> False
+
+instance Subst Type Type Equation where
+  subst l r (a :~ b) = do a' <- subst l r a
+                          b' <- subst l r b
+                          return $ a' :~ b'
+
+instance Subst Type Type StackType where
+  subst l r (as :# a) = do as' <- mapM (subst l r) as
+                           return (as' :# a)
+
+instance Subst Int StackType Type where
+  subst n a t | n `stackOccursIn` a = fail "Cyclic type"
+              | otherwise = case t of
+                  Fun b c -> Fun <$> (subst n a b) <*> (subst n a c)
+                  _ -> return t
+
+instance Subst Int StackType StackType where
+  subst n (as :# a) (xs :# x) = do
+    xs' <- mapM (subst n (as :# a)) xs
+    return $ if x == n
+      then (xs' ++ as) :# a
+      else xs' :# x
+
+instance Subst Int StackType Equation where
+  subst n s (a :~ b) = (:~) <$> subst n s a <*> subst n s b
+
+addEqn :: Equation -> Unifier ()
+addEqn e = Unifier (modify (++[e]))
+
+getEqns :: Unifier [Equation]
+getEqns = Unifier get
+
+clearEqns :: Unifier ()
+clearEqns = Unifier (put [])
