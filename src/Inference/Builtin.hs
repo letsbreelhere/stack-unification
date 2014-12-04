277ba25790fcb09fa30be899ed5faf49f1f56e10
Bree Stanwyck <bree@bignerdranch.com>
2014-12-04 14:14:05 -0500

    Add `quote` builtin word

--- src/Inference/Builtin.hs
+++ src/Inference/Builtin.hs
@@ -6,17 +6,18 @@ import Inference.Types
 builtinTypes :: [(String, Inference Type)]
 builtinTypes = [ ("dup", dup_)
                , ("i", i_)
-               , ("pop", pop_)
+               , ("drop", drop_)
                , ("swap", swap_)
+               , ("quote", quote_)
                ]
 
 dup_ :: Inference Type
 dup_ = do v <- freshVar
           [v] --> [v, v]
 
-pop_ :: Inference Type
-pop_ = do v <- freshVar
-          [v] --> []
+drop_ :: Inference Type
+drop_ = do v <- freshVar
+           [v] --> []
 
 swap_ :: Inference Type
 swap_ = do v <- freshVar
@@ -27,4 +28,9 @@ i_ :: Inference Type
 i_ = do s <- freshStackVar
         s' <- freshStackVar
         let top = Fun ([] :# s) ([] :# s')
-        return (Fun ([top] :# s) ([] :# s'))
+        return $ Fun ([top] :# s) ([] :# s')
+
+quote_ :: Inference Type
+quote_ = do v <- freshVar
+            f <- [] --> [v]
+            [v] --> [f]
